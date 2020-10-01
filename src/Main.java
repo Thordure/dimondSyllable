36959823ab92c2945cca9db777b46d9d15f5acbf
Thordure <Thordure@gmail.com>
2020-10-01 13:35:20 +0200

    start test

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//p4.11 page 190
+
+public class Main {
+
+    public static void main (String args []) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Please insert word here");
+
+
+
+    }
+
+
+
+}
